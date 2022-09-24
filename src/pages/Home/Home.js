0ecf339427a6965e6605712a006937c9f9971dc4
Rashidaakter1 <rashidaakterchadni@gmail.com>
2022-09-24 23:45:01 +0600

    initialize a project

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Contact from "./Contact/Contact";
+
+const Home = () => {
+  return (
+    <>
+      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+
+      <Contact />
+    </>
+  );
+};
+
+export default Home;
